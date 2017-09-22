004aeb7b336e944e4701154414e6cb846c527ec2
wangliya <1096505177@qq.com>
2017-09-22 19:14:07 +0800

    toUpdate

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
-/* 
-* @Author: Marte
-* @Date:   2017-09-22 14:43:37
-* @Last Modified by:   Marte
-* @Last Modified time: 2017-09-22 14:49:59
-*/
-
-$(document).ready(function(){
-    
-});
+//本地安装gulp
+//require():commonjs的规范-->同步
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+//编译sass
+//利用gulp任务来编译
+//创建gulp任务：gulp.task()
+gulp.task('compileSass',function(){
+    //查找sass文件
+    //匹配文件成功后，返回文档流
+    gulp.src('./src/')
+})
