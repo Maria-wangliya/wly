2ee89362ec7299ae3bb9f89e13efbc43c8ffb79b
wangliya <1096505177@qq.com>
2017-09-24 11:41:44 +0800

    1.上传修改的gulpfile文件

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
-//本地安装gulp
-//require():commonjs的规范-->同步
+//本地安装gulp:为了在此处引入gulp
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-//编译sass
-//利用gulp任务来编译
-//创建gulp任务：gulp.task()
-gulp.task('compileSass',function(){
-    //查找sass文件
-    //匹配文件成功后，返回文档流
-    gulp.src('./src/')
-})
+// 利用gulp任务来编译
+gulp.task('project',function(){
+    // 查找sass文件
+    gulp.src('./src/sass/*.scss')
+        // 编译sass文件
+        .pipe(sass().on('error', sass.logError))
+        // 输出文件到硬盘
+        .pipe(gulp.dest('./src/css/'));
+});
+// 监听sass文件修改
+gulp.task('watchSass',function(){
+    // 监听home.scss文件，如果有修改,则自动自动project任务
+    gulp.watch('./src/**/*.scss',['project']);
+});
